0a471dd5e0170da7eddddd93cfdde2fea38faf74
Victor Tishkevich <victish9@gmail.com>
2026-04-24 15:44:40 -0400

    Created ablation dataset generator, and renamed configs.py

---
+++ src/model_configs.py
@@ -0,0 +1,8 @@
+MODEL_NAME = "google/gemma-4-E4B-it" # "google/gemma-4-E4B-it" "allenai/Molmo2-4B"
+DATASET_PATH = "../data/coco-dataset/val2017"
+ANNOTATIONS_PATH = "../data/coco-dataset/annotations/captions_val2017.json"
+RESULTS_PATH = "../results/model-results/" + MODEL_NAME.split("/")[-1] + "_results.json"
+
+# for CIDEr and SPICE evaluation, need to keep output short
+PROMPT = "Describe this image in one brief sentence." #"Describe the image in detail." "long caption 70: Describe everything you see in detail, including specific descriptions of spatial positioning and relationships"
+MAX_NEW_TOKENS = 2048
